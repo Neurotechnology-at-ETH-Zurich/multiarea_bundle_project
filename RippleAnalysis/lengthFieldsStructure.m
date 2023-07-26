6661acd745e8ce08d89e81f473721d39bb6b45d6
gombpeti <pgombkoto@ethz.ch>
2023-07-26 14:31:28 +0200

    Update lengthFieldsStructure.m

--- RippleAnalysis/lengthFieldsStructure.m
+++ RippleAnalysis/lengthFieldsStructure.m
@@ -1,1 +1,16 @@
-   lengths = sum(unique(arrayfun(@(x) size(Cells(x).individual_trials,1), 1:numel(Cells))))
+(sum([Cells(find(ismember({Cells.Structre},'IL'))).clusterID_kmeans]==1)./sum([ismember({Cells.Structre},'IL')])).*100   
+
+
+(sum([Cells(find(ismember({Cells.Structre},'IL'))).clusterID_kmeans]==1)./sum([ismember({Cells.Structre},'IL')])).*100   
+
+lengths = sum(unique(arrayfun(@(x) size(Cells(x).individual_trials,1), 1:numel(Cells))))
+
+
+(sum([Cells(find(ismember({Cells.Structre},'PrL'))).clusterID_kmeans]==1)./sum([ismember({Cells.Structre},'PrL')])).*100   
+
+
+T = struct2table(statistic.RSC_dHP.mean,)
+
+
+
+(sum([Cells(find(ismember({Cells.Structre},'IL'))).clusterID_kmeans]==1)./sum([ismember({Cells.Structre},'IL')])).*100   
